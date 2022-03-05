43ddaff838dcecc5b2aaf77fadec3390cb4bb92a
João Silva <75593113+Z4Dev@users.noreply.github.com>
2022-03-05 23:09:48 +0000

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,38 @@
+/*
+--------------------------------------------------------
+                  Z4Dev - JavaScript
+--------------------------------------------------------
+*/ 
+
+ 
+/*
+      --------------------------------------------------------
+                 JavaScript - Array/Variável Composta
+      --------------------------------------------------------
+*/ 
+
+var array = ["A","B","C","D","E","F"]
+
+// Como printar o primeiro elemento? //
+
+console.log(array[0])
+
+// Como printar todos os elementos? //
+
+console.log(array)
+
+// Como procurar um elemento? //
+
+const procurarvalor = array.indexOf("A") // Vai procurar o elemento "A", caso o elemento procurado não exista vai printar -1
+console.log(procurarvalor)
+
+// Como criar um novo elemento? //
+const adicionarvalor = array.push("G")
+console.log(array)
+
+// Como contar todos os elementos existentes ? //
+const teste = array.length
+console.log(teste)
+
+// Mais Em Breve //
+
